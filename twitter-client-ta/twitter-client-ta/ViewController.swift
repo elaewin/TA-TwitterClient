b9e661d7e8609368df4855f9fb41b3a513a8ea4a
Erica Winberry <elaewin@gmail.com>
2017-03-20 09:52:04 -0700

    create xcode project

---
+++ twitter-client-ta/twitter-client-ta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter-client-ta
+//
+//  Created by Erica Winberry on 3/20/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
